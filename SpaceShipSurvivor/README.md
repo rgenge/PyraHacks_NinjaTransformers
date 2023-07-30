94d29c2420089d9bfb19e13525780184bbe0cceb
rgenge <rgenge@terra.com.br>
2023-07-30 15:01:23 -0400

    Space new

--- SpaceShipSurvivor/README.md
+++ SpaceShipSurvivor/README.md
@@ -1,1 +1,2 @@
-Very simple game where you do have to get the energy in the space and do not get hit by the rocks , the more energy the fasted points you get
+Very simple game where you have to make more scores and after you lose you will always learn some curiosity about the space!
+A fun way for kids to learn.
--- SpaceShipSurvivor/main.py
+++ SpaceShipSurvivor/main.py
@@ -2,10 +2,12 @@
 import pygame
 import random
 import time
+
 pygame.init()
 start_time = time.time()
+quizz_list = ['Did you know, there is no atmosphere in space, which means that sound has no medium or way to travel to be heard.', 'Did you know ,Venus is the hottest planet in the solar system and has an average surface temperature of around 450° C. ', 'Did you know, While the entire suit costs a cool $12m, 70percent of that cost is for the backpack and control module.', 'Did you know , The Sun accounts for 99.86 percent of the mass in our solar system with a mass of around 330,000 times that of Earth. ', 'Did you know ,Just as colors are made more dramatic in sunsets on Earth, sunsets on Mars, according to NASA,  would appear bluish to human observers watching from the red planet.', 'Did you know, The universe extends far beyond our own galaxy, scientists estimate the universe contains approximately 1,000,000,000,000,000,000,000,000 stars, or a septillion.', 'Did you know, there is a planet made of diamonds twice the size of earth The "super earth,"is made of graphite and diamond. ', ' Did you know, The Moon has no atmosphere, which means there is no wind to erode the surface .This means the footprint will be there for millions of years.', 'Did you know, The largest known asteroid is 965 km (600 mi) wide.', 'Did you know, The Moon was once a piece of the Earth.']
 # Create the screen.
-screen = pygame.display.set_mode((1500, 800))
+screen = pygame.display.set_mode((1600, 800))
 # Load images.
 player_image = pygame.image.load("player.png")
 background_image = pygame.image.load("background.png")
@@ -59,14 +61,42 @@ while True:
     obstacle2.x += (index * 4)
     obstacle3.x += (index * 4)
     # Check for collision.
+
     if player.colliderect(obstacle1):
+        quiz = random.choice(quizz_list)
+        font = pygame.font.SysFont("Arial", 15)
+        game_over_text = font.render(quiz, True, (255, 255, 255))
+        text_rect = game_over_text.get_rect()
+        text_rect.centerx = 800
+        text_rect.centery = 300
+        screen.blit(game_over_text, text_rect)
+        pygame.display.update()
         print("You lost!")
+        time.sleep(5)
         break
     if player.colliderect(obstacle2):
+        quiz = random.choice(quizz_list)
+        font = pygame.font.SysFont("Arial", 15)
+        game_over_text = font.render(quiz, True, (255, 255, 255))
+        text_rect = game_over_text.get_rect()
+        text_rect.centerx = 800
+        text_rect.centery = 300
+        screen.blit(game_over_text, text_rect)
+        pygame.display.update()
         print("You lost!")
+        time.sleep(5)
         break
     if player.colliderect(obstacle3):
+        quiz = random.choice(quizz_list)
+        font = pygame.font.SysFont("Arial", 15)
+        game_over_text = font.render(quiz, True, (255, 255, 255))
+        text_rect = game_over_text.get_rect()
+        text_rect.centerx = 800
+        text_rect.centery = 300
+        screen.blit(game_over_text, text_rect)
+        pygame.display.update()
         print("You lost!")
+        time.sleep(5)
         break
     if player.colliderect(energy):
         bonus_points +=1
@@ -79,9 +109,9 @@ while True:
     screen.blit(background_image, background)
     screen.blit(obstacle_image, obstacle1)
     screen.blit(obstacle_image, obstacle2)
+
     screen.blit(obstacle_image, obstacle3)
     screen.blit(energy_image, energy)
     screen.blit(player_image, player)
     screen.blit(score, (1400,10))
-    # Update the display.
     pygame.display.update()
